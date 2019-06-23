c3e4d52052a4e61ede38773b9eb856fc389eaf4b
Renato Ferreira Germano <Renatoawk@gmail.com>
2019-06-23 15:28:53 -0300

    Response para lista e fragmentos

--- app/src/main/java/com/bsi/fbd/minisiga/gui/adm/BlocoAdmFragment.java
+++ app/src/main/java/com/bsi/fbd/minisiga/gui/adm/BlocoAdmFragment.java
@@ -2,46 +2,27 @@ package com.bsi.fbd.minisiga.gui.adm;
 
 import android.content.Context;
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
-
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.bsi.fbd.minisiga.R;
 import com.bsi.fbd.minisiga.modelo.Bloco;
-import com.bsi.fbd.minisiga.modelo.Connection;
 import com.bsi.fbd.minisiga.modelo.Faculdade;
-import com.bsi.fbd.minisiga.modelo.QuickAdapter;
+import com.bsi.fbd.minisiga.modelo.Response;
 import com.bsi.fbd.minisiga.modelo.User;
-import com.chad.library.adapter.base.BaseQuickAdapter;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class BlocoAdmFragment extends Fragment {
 
-    private RequestQueue requestQueue;
     private ArrayList<Bloco> resultado = new ArrayList<>();
     private RecyclerView recyclerView;
-    private QuickAdapter quickAdapter;
     private Context context;
     private View view;
 
@@ -60,94 +41,11 @@ public class BlocoAdmFragment extends Fragment {
         recyclerView = view.findViewById(R.id.blocoRecyclerAdm);
         recyclerView.setLayoutManager(new LinearLayoutManager(context));
         resultado.clear();
-        popular();
+        Response response = new Response("getblocos.php", context, null, recyclerView);
+        Map<String, String> params = new HashMap<>();
+        Faculdade faculdade = (Faculdade) User.getCurrentUser();
+        params.put("sigla_faculdade",faculdade.getSigla());
+        response.run(params);
         return view;
     }
-
-    private void setUpAdapter() {
-        quickAdapter = new QuickAdapter(R.layout.layout_item_recycler, resultado);
-        quickAdapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
-            @Override
-            public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
-                ////// mandar pra tela de detalhes
-            }
-        });
-
-        quickAdapter.setOnItemChildClickListener(new BaseQuickAdapter.OnItemChildClickListener() {
-            @Override
-            public void onItemChildClick(BaseQuickAdapter adapter, View view, int position) {
-                if (view.getId() == R.id.edit){
-                    ////// mandar pra tela de edição
-                    Toast.makeText(context,"Clicou em editar", Toast.LENGTH_SHORT).show();
-                } else {
-                    ////// apagar
-                    Toast.makeText(context,"Clicou em apagar", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-        recyclerView.setAdapter(quickAdapter);
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
-
-    private void popular() {
-        requestQueue = Volley.newRequestQueue(getContext());
-
-        String url = Connection.getUrl() + "getblocos.php";
-
-        Response.Listener<String> responseListener = new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                try {
-                    JSONObject jsonObject = new JSONObject(response);
-                    boolean error = jsonObject.getBoolean("erro"); //Esse erro é o mesmo passado lá no php
-                    if (!error) {
-                        int qtd = jsonObject.getInt("qtd");
-                        for (int i = 0; i < qtd ; i++) {
-                            JSONObject item = jsonObject.getJSONObject(String.valueOf(i));
-                            Bloco bloco = new Bloco();
-                            bloco.setSiglaFaculdade(item.getString("sigla_faculdade"));
-                            bloco.setArea(item.getString("area"));
-                            bloco.setCodigo(Integer.parseInt(item.getString("codigo")));
-                            bloco.setTipo(item.getString("tipo"));
-                            resultado.add(bloco);
-                        }
-                        setUpAdapter();
-
-                    } else {
-                        Toast.makeText(context, "Erro 1", Toast.LENGTH_LONG).show();
-                    }
-                } catch (JSONException e) {
-                    Toast.makeText(context, "Erro 2 ", Toast.LENGTH_LONG).show();
-                }
-
-            }
-        };
-
-        Response.ErrorListener errorListener = new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                String motivo = error.getMessage();
-                Toast.makeText(context, "Erro desconhecido: " + motivo, Toast.LENGTH_LONG).show();
-            }
-        };
-
-        StringRequest stringRequest = new StringRequest(
-                Request.Method.POST, url, responseListener, errorListener) {
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                Map<String, String> params = new HashMap<>();
-                Faculdade faculdade = (Faculdade) User.getCurrentUser();
-                params.put("sigla_faculdade", faculdade.getSigla());
-                return params;
-            }
-        };
-        requestQueue.add(stringRequest);
-    }
-
-
 }
--- app/src/main/java/com/bsi/fbd/minisiga/modelo/Response.java
+++ app/src/main/java/com/bsi/fbd/minisiga/modelo/Response.java
@@ -1,7 +1,12 @@
 package com.bsi.fbd.minisiga.modelo;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.view.View;
 import android.widget.Toast;
+
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -10,8 +15,12 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.bsi.fbd.minisiga.R;
+import com.chad.library.adapter.base.BaseQuickAdapter;
+
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.Map;
 
 public class Response {
@@ -22,11 +31,22 @@ public class Response {
     private StringRequest stringRequest;
     private Context context;
     private JSONObject jsonObject;
+    private Activity edit;
+    private RecyclerView recyclerView;
+    private QuickAdapter adapter;
+
 
     public Response(String php, final Context context) {
         this.url = Connection.getUrl() + php;
         requestQueue = Volley.newRequestQueue(context);
         this.context = context;
+    }
+    public Response(String php, final Context context, Activity edit, RecyclerView recyclerView){
+        this.url = Connection.getUrl() + php;
+        requestQueue = Volley.newRequestQueue(context);
+        this.context = context;
+        this.edit = edit;
+        this.recyclerView = recyclerView;
     }
 
     public JSONObject getJsonObject() {
@@ -44,7 +64,35 @@ public class Response {
                         jsonObject = new JSONObject(response);
                         boolean error = jsonObject.getBoolean("erro"); //Esse erro é o mesmo passado lá no php
                         if (!error) {
-                            Toast.makeText(context.getApplicationContext(), "Operação realizada", Toast.LENGTH_LONG).show();
+                            if (recyclerView != null){
+                                String tipo = jsonObject.getString("tipo");
+                                if (tipo.equals("bloco")){
+
+                                    int qtd = jsonObject.getInt("qtd");
+                                    ArrayList<Bloco> resultado = new ArrayList<>();
+                                    for (int i = 0; i < qtd ; i++) {
+                                        JSONObject item = jsonObject.getJSONObject(String.valueOf(i));
+                                        Bloco bloco = new Bloco();
+                                        bloco.setSiglaFaculdade(item.getString("sigla_faculdade"));
+                                        bloco.setArea(item.getString("area"));
+                                        bloco.setCodigo(Integer.parseInt(item.getString("codigo")));
+                                        bloco.setTipo(item.getString("tipo"));
+                                        resultado.add(bloco);
+                                    }
+                                    adapter = new QuickAdapter(R.layout.layout_item_recycler, resultado);
+                                    setUpAdapter();
+
+                                } ///// escrever para os outros tipos
+
+
+
+
+
+
+
+                            } else {
+                                Toast.makeText(context.getApplicationContext(), "Operação realizada", Toast.LENGTH_LONG).show();
+                            }
                         } else {
                             Toast.makeText(context.getApplicationContext(), "Erro 1", Toast.LENGTH_LONG).show();
                         }
@@ -67,11 +115,6 @@ public class Response {
                     Request.Method.POST, url, responseListener, errorListener) {
                 @Override
                 protected Map<String, String> getParams() throws AuthFailureError {
-                    //Exemplo da estrutura do dicionario
-                    //params.put("codigo", codigoLayout.getEditText().getText().toString().trim());
-                    //params.put("area", areaLayout.getEditText().getText().toString().trim());
-                    //params.put("tipo", tipoLayout.getEditText().getText().toString().trim());
-                    //params.put("sigla_faculdade", faculdade.getSigla() );
                     return params;
                 }
             };
@@ -79,5 +122,28 @@ public class Response {
         } else {
             Toast.makeText(context.getApplicationContext(), context.getApplicationContext().getString(R.string.IP_required), Toast.LENGTH_LONG);
         }
+    }
+
+    private void setUpAdapter() {
+        adapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
+                ////// mandar pra tela de detalhes
+            }
+        });
+
+        adapter.setOnItemChildClickListener(new BaseQuickAdapter.OnItemChildClickListener() {
+            @Override
+            public void onItemChildClick(BaseQuickAdapter adapter, View view, int position) {
+                if (view.getId() == R.id.edit){
+                    ////// mandar pra tela de edição
+                    Toast.makeText(context,"Clicou em editar", Toast.LENGTH_SHORT).show();
+                } else {
+                    ////// apagar
+                    Toast.makeText(context,"Clicou em apagar", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        recyclerView.setAdapter(adapter);
     }
 }
