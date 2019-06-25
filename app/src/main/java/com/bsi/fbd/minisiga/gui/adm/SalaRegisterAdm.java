28a8ef534643b8f5054510f7716f7c381c7aa199
Renato Ferreira Germano <Renatoawk@gmail.com>
2019-06-25 13:06:43 -0300

    criação das activities de cadastro sala e curso

---
+++ app/src/main/java/com/bsi/fbd/minisiga/gui/adm/SalaRegisterAdm.java
@@ -0,0 +1,16 @@
+package com.bsi.fbd.minisiga.gui.adm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bsi.fbd.minisiga.R;
+
+public class SalaRegisterAdm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sala_register_adm);
+    }
+}
